ccd5d7b16a721c2c7b1105bc0b743dc97fa9f4c2
Pete George <georgepj@vcu.edu>
2019-11-07 12:45:56 -0500

    Made nicer, externalized methods.

--- project4main.py
+++ project4main.py
@@ -6,6 +6,57 @@ Created on Tue Nov  5 17:56:19 2019
 """
 import porterstemmer
 p = porterstemmer.PorterStemmer()
+
+def tokenize (anArray):
+    splitWordsArray = []
+    for _ in range(0,len(sentences)):
+        temp = []
+        mystr = sentences[_].lower()
+        for c in mystr[ : : 1]:
+            if (not c.isalpha() and c != " "):
+                mystr = mystr.replace(c,"")
+            for x in nums:
+                mystr = mystr.replace(x,"")       
+            temp = mystr.split()
+        splitWordsArray.append(temp)
+    return splitWordsArray
+
+def backToSentences (anArray):
+    toReturn = []
+    for aSentence in anArray:
+        string = ""
+        for aWord in aSentence:
+            string = string + aWord + " "
+        string = string[:-1]
+        if(string):
+            toReturn.append(string)
+    return toReturn
+
+def removeStopWords (anArray):
+    toReturn = []
+    for aSentence in anArray:
+        tempSentence = aSentence
+        for sw in stopwords:
+            for aWord in tempSentence:
+                if aWord == sw:
+                    tempSentence.remove(aWord)
+        if(tempSentence):
+            toReturn.append(tempSentence)
+    return toReturn
+    
+def portStem (anArray):
+    toReturn = []
+    for aSentence in anArray:
+        tempSentence = aSentence
+        sent = []
+        for aWord in tempSentence:
+            sent.append(p.stem(aWord,0,len(aWord)-1))
+        toReturn.append(sent)
+    return toReturn
+    
+            
+
+
 with open('stop_words.txt') as fp1:
     stopwords = fp1.read().splitlines()   #Maybe needs to be outside of open
 
@@ -17,38 +68,11 @@ with open('sentences.txt') as fp:
         sentences.append(line)
         line = fp.readline()
         
-splitWords = []
-for _ in range(0,len(sentences)):
-    temp = []
-    mystr = sentences[_].lower()
-    for c in mystr[ : : 1]:
-        if (not c.isalpha() and c != " "):
-            mystr = mystr.replace(c,"")
-    for x in nums:
-        mystr = mystr.replace(x,"")       
-    temp = mystr.split()
-    splitWords.append(temp)
-    
-tempSentence = []
-noStopWords = []
-for aSentence in splitWords:
-    tempSentence = aSentence
-    for sw in stopwords:
-        for aWord in tempSentence:
-            if aWord == sw:
-                tempSentence.remove(aWord)
-    if(tempSentence):
-        noStopWords.append(tempSentence)
+splitWords = tokenize(sentences)
+noStopWords = removeStopWords(splitWords)
+tokenPorterStemmed = portStem(noStopWords)
+sentencePorterStemmed = backToSentences(tokenPorterStemmed)
 
-backToStrings = []
-for aSentence in noStopWords:
-    string = ""
-    for aWord in aSentence:
-        string = string + aWord + " "
-    string = string[:-1]
-    if(string):
-        backToStrings.append(string)
-print(backToStrings)
-st = "hellos"
-sentence = p.stem(st,0,len(st)-1)
-print(sentence)
+
+#for aSentence in backToStrings:
+    
